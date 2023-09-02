33259142e87476ec53c47315e028925630c5d524
ikeda <daikenkai3542@gmail.com>
2023-09-02 23:12:11 +0900

    Add jest Modifiers

--- jest/basic.test.ts
+++ jest/basic.test.ts
@@ -2,4 +2,17 @@ describe('基本的な使い方', () => {
   test('Matchers', () => {
     expect(1 + 1).toBe(2);
   });
+
+  test('Modifiers', async () => {
+    // not
+    expect(1 + 1).not.toBe(3);
+
+    // promise
+    expect(Promise.resolve('resolved!')).resolves.toBe('resolved!');
+    expect(Promise.reject(new Error('rejected...'))).rejects.toThrow('rejected...');
+
+    // async/await
+    await expect(Promise.resolve('resolved!')).resolves.toBe('resolved!');
+    await expect(Promise.reject(new Error('rejected...'))).rejects.toThrow('rejected...');
+  });
 });
